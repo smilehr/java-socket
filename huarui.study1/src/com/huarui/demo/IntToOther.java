4aec66a2a36d30305d92d383ac78f6641334551c
huarui <huarui@succez.com>
2017-07-04 10:19:33 +0800

    修改常量名为HEX_CHARS,优化转换算法，直接由十进制转换为16进制

--- huarui.study1/src/com/huarui/demo/IntToOther.java
+++ huarui.study1/src/com/huarui/demo/IntToOther.java
@@ -12,65 +12,59 @@ public class IntToOther {
 	/**
 	 * 字符数组，二进制转16进制时进行数值转换
 	 */
-	private final static char[] HEX_VALUE = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+	private final static char[] HEX_CHARS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
 			'f' };
 
 	/**
-	 * 将负整数转换成二进制数组
+	 * 将负整数转换成byte数组
 	 * @param obj,传入的整数数
-	 * @param int_hex[] 返回的二进制数组
+	 * @param bytes[] 返回的byte数组
 	 * @return
 	 */
-	private static int[] negIntToBinary(int obj, int[] int_hex) {
-		//取反码
-		for (int i = 31; i >= 0; i--) {
-			if (obj == 0) {
-				int_hex[i] = 1;
-			}
-			else {
-				int_hex[i] = 0;
-				obj >>= 1;
-			}
+	public static byte[] negIntToBinary(int obj, byte[] bytes) {
+		int rem;
+		boolean isCarry = true;
+		for (int i = 7; i >= 0; i--) {
+			rem = obj % 16;
+			bytes[i] = (byte) (rem ^ 0xF);
+			obj >>= 4;
+			if(isCarry){
+				if(bytes[i] == 15){
+					bytes[i] = 0;
+				}
+				else{
+					bytes[i]++;
+					isCarry = false;
+				}
+			}			
 		}
-		//反码加一得补码
-		for (int i = 31; i >= 0; i--) {
-			if (int_hex[i] == 1) {
-				int_hex[i] = 0;
-			}
-			if (int_hex[i] == 0) {
-				int_hex[i] = 1;
-				break;
-			}
-		}
-		return int_hex;
+		return bytes;
 	}
 
 	/**
-	 * 将正整数转换成二进制数组
+	 * 将正整数转换为byte数组
 	 * @param obj,传入的整数
-	 * @param int_hex[] 返回的二进制数组
+	 * @param bytes[] 返回的二进制数组
 	 * @return
 	 */
-	private static int[] posIntToBinary(int obj, int[] int_hex) {
-		for (int i = 31; i >= 0; i--) {
+	private static byte[] posIntToBinary(int obj, byte[] bytes) {
+		for (int i = 7; i >= 0; i--) {
 			if (obj == 0) {
-				int_hex[i] = 0;
+				bytes[i] = 0;
 			}
 			else {
-				int_hex[i] = obj % 2;
-				obj = obj >> 1;
+				bytes[i] = (byte) (obj % 16);
+				obj = obj >> 4;
 			}
 		}
-		return int_hex;
+		return bytes;
 	}
 
 	/**
 	  * 将整数转换成十六进制字符串
-	  * @param int_hex，存放obj转换的二进制数值
+	  * @param bytes，存放obj转换的十六进制数值
 	  * @param obj,传入的整数数值
-	  * @param sum,二进制转换成十六进制时临时变量
 	  * @param hex，转换好的十六进制字符串
-	  * @param key_hex,预定义数组，用来处理字符串拼接
 	  * <pre>
 	  * IntToOther.intToHex(2147483647) = "7fffffff";
 	  * IntToOther.intToHex(-3) = "fffffffd";
@@ -79,36 +73,34 @@ public class IntToOther {
 	  * </pre>
 	  * @return
 	  */
-	public StringBuilder intToHex(int obj) {
+	public String intToHex(int obj) {
 
 		StringBuilder hex = new StringBuilder(8);
 		//当传入的十进制数为负数的情况
 		if (obj == Integer.MIN_VALUE) {
 			hex.append("80000000");
-			return hex;
+			return hex.toString();
 		}
-		int[] int_hex = new int[32];
-		int sum;
-
+		byte[] bytes = new byte[8];
 		//获取整数转换成二进制后的数组
 		if (obj < 0) {
-			int_hex = negIntToBinary(Math.abs(obj), int_hex);
+			bytes = negIntToBinary(Math.abs(obj), bytes);
 		}
 		else {
-			int_hex = posIntToBinary(obj, int_hex);
+			bytes = posIntToBinary(obj, bytes);
 		}
 		//将二进制数组int_hex转换成十六进制字符串hex
-		for (int i = 0; i < 32; i += 4) {
-			sum = int_hex[i + 3] + (int_hex[i + 2] << 1) + (int_hex[i + 1] << 2) + (int_hex[i] << 3);
-			if (sum == 0 && hex.length() == 0) {
-				if (i == 28) {
+		for (int i = 0; i < 8; i++) {
+//			System.out.print(bytes[i]);
+			if (bytes[i] == 0 && hex.length() == 0) {
+				if(i == 7){
 					hex.append('0');
 				}
 				continue; //进行首位去0处理
 			}
 			//字符串拼接
-			hex.append(HEX_VALUE[sum]);
+			hex.append(HEX_CHARS[bytes[i]]);
 		}
-		return hex;
+		return hex.toString();
 	}
 }
