ca917cdd0cba30018ae1310bf69575e453b67ebe
huarui <huarui@succez.com>
2017-07-04 14:03:01 +0800

    完善文件读取测试样例

--- huarui.study1/test/com/huaui/test/FileReadTest.java
+++ huarui.study1/test/com/huaui/test/FileReadTest.java
@@ -64,18 +64,47 @@ public class FileReadTest extends TestCase {
 		String path_txt = "D:\\test.txt";
 		String path_txt1 = "D:\\test1.txt";
 		String path_mp3 = "D:\\test.mp3";
-		//相同内容相同编码
-//		fis_txt = creatFile(path_txt, "utf-8", "测试用例！");
-//		fis_txt1 = creatFile(path_txt1, "utf-8", "测试用例！");
+		String path_pdf = "D:\\test.pdf";
+		String path_doc = "D:\\test.doc";
+		fis_txt = creatFile(path_txt, "utf-8", "测试用例1！");
+		fis_txt1 = creatFile(path_txt1, "utf-8", "测试用例1！");
+		fis_mp3 = creatFile(path_mp3, "utf-8", "测试用例3！");
+		
+		//测试不同扩展名文件读取
+		File fis_pdf = creatFile(path_pdf,"utf-8","测试用例！");
+		Assert.assertEquals(fis_pdf.length(), FileRead.file2buf(fis_pdf).length);
+		File fis_doc = creatFile(path_doc,"utf-8","测试用例！");
+		Assert.assertEquals(fis_pdf.length(), FileRead.file2buf(fis_doc).length);
+		Assert.assertEquals(fis_txt.length(), FileRead.file2buf(fis_txt).length);
+		Assert.assertEquals(fis_mp3.length(), FileRead.file2buf(fis_mp3).length);
+		//相同内容相同编码名字不同
+		
+		Assert.assertArrayEquals(FileRead.file2buf(fis_txt), FileRead.file2buf(fis_txt1));
 		
 		//相同内容不同编码
-//		fis_txt = creatFile(path_txt, "utf-8", "测试用例！");
-//		fis_txt1 = creatFile(path_txt1, "GBK", "测试用例！");
+		fis_txt = creatFile(path_txt, "utf-8", "测试用例2！");
+		fis_txt1 = creatFile(path_txt1, "GBK", "测试用例2！");
+		Assert.assertNotEquals(FileRead.file2buf(fis_txt), FileRead.file2buf(fis_txt1));
 		
 		//相同内容相同编码不同扩展名
-//		fis_txt = creatFile(path_txt, "utf-8", "测试用例！");
-//		fis_mp3 = creatFile(path_mp3, "utf-8", "测试用例！");
-		
+		fis_txt = creatFile(path_txt, "utf-8", "测试用例3！");		
 		Assert.assertArrayEquals(FileRead.file2buf(fis_txt), FileRead.file2buf(fis_mp3));
+		
+		//测试目录，非文件情况
+		File not_file = new File("D:\\");
+		Assert.assertArrayEquals(null, FileRead.file2buf(not_file));
+		
+		//测试大文件的情况
+		File big_file = new File("D:\\Office 2013(64位VOL版)+激活工具.rar");
+		Assert.assertEquals(big_file.length(), FileRead.file2buf(big_file).length);
+		
+		//测试文件不存在的情况
+		File no_file = new File("D:\\1.txt");
+		Assert.assertArrayEquals(null, FileRead.file2buf(no_file));
+		
+		//测试文件过大的情况
+		File eor_file = new File("D:\\Applications.rar");
+		Assert.assertArrayEquals(null, FileRead.file2buf(eor_file));
+		
 	}
 }
