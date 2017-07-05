43dd23aa35974e1befc4dc7381574383cd59e30a
huarui <huarui@succez.com>
2017-07-05 11:20:22 +0800

    第二题修改，优化代码的查看

--- huarui.study1/test/com/huaui/test/TreeTest.java
+++ huarui.study1/test/com/huaui/test/TreeTest.java
@@ -1,5 +1,7 @@
 package com.huaui.test;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.huarui.demo.Tree;
@@ -14,6 +16,54 @@ import junit.framework.TestCase;
  * @createdate 2017年7月3日
  */
 public class TreeTest extends TestCase {
+	/**
+	 * 测试树的遍历
+	 * @param tree,测试用生成的树,结构为
+	 * <pre>
+	 * 						 			1
+	 * 				      		2					  3
+	 * 			     4	              5 		6   	    7
+	 * 	 	    8          9      10    11   12    13    14    15
+	 * 		16    17   18    19 20 21 22 23 24 25 26 27 28 29 30 31
+	 *    32 33 34 35 36 37 38 39
+	 * </pre>
+	 */
+	@Test
+	public void testTreeLevel() {
+		TNode tree = new TNode();
+		tree.value = 1;
+		InitTree(tree);
+		//树不为空且层数大于0小于树的高度
+		assertEquals("23", Tree.treeLevel(tree, 2));
+
+		//树不为空且层数大于0小于树的高度
+		assertEquals("1", Tree.treeLevel(tree, 1));
+
+		//树不为空且层数等于树的高度
+		assertEquals("3233343536373839", Tree.treeLevel(tree, 6));
+	}
+
+	/**
+	 * 测试特殊情况
+	 * @throws IOException 
+	 */
+	@Test
+	public void testTreeLevelSpecial() throws IOException {
+
+		TNode tree = new TNode();
+		tree.value = 1;
+		InitTree(tree);
+		TNode tree1 = null;
+
+		//树不为空，层数为0
+		assertEquals(null, Tree.treeLevel(tree, 0));
+
+		//树不为空且层数大于树的高度
+		assertEquals("", Tree.treeLevel(tree, 8));
+
+		//树为空
+		assertEquals(null, Tree.treeLevel(tree1, 3));
+	}
 
 	/**
 	 * 该类定义二叉树的结构
@@ -37,11 +87,9 @@ public class TreeTest extends TestCase {
 		if (tn.value < 20) {
 			TNode t1 = new TNode();
 			TNode t2 = new TNode();
-
 			t1.value = tn.value * 2;
 			tn.left = t1;
 			InitTree(t1);
-
 			t2.value = tn.value * 2 + 1;
 			tn.right = t2;
 			InitTree(t2);
@@ -50,43 +98,6 @@ public class TreeTest extends TestCase {
 			tn.left = null;
 			tn.right = null;
 		}
-	}
-	
-	/**
-	 * 测试树的遍历
-	 * @param tree,测试用生成的树,结构为
-	 * <pre>
-	 * 						 			1
-	 * 				      		2					  3
-	 * 			     4	              5 		6   	    7
-	 * 	 	    8          9      10    11   12    13    14    15
-	 * 		16    17   18    19 20 21 22 23 24 25 26 27 28 29 30 31
-	 *    32 33 34 35 36 37 38 39
-	 * </pre>
-	 */
-	@Test
-	public void testTreeLevel() {
-		TNode tree = new TNode();
-		tree.value = 1;
-		InitTree(tree);
-		TNode tree1 = null;
-		//树不为空且层数大于0小于树的高度
-		assertEquals("23", Tree.treeLevel(tree, 2));
-		
-		//树不为空且层数大于0小于树的高度
-		assertEquals("1", Tree.treeLevel(tree, 1));
-		
-		//树不为空且层数等于树的高度
-		assertEquals("3233343536373839", Tree.treeLevel(tree, 6));
-		
-		//树不为空，层数为0
-		assertEquals(null, Tree.treeLevel(tree, 0));
-		
-		//树不为空且层数大于树的高度
-		assertEquals("", Tree.treeLevel(tree, 8));
-		
-		//树为空
-		assertEquals(null, Tree.treeLevel(tree1, 3));
 	}
 
 }
