3aac55ca7b54d1ce49a3843e0c540df6dd837e87
huarui <huarui@succez.com>
2017-07-17 14:39:57 +0800

    单元测试类删除httpserver测试

--- huarui.study2/test/huarui/study2/TestHttpServer.java
+++ huarui.study2/test/huarui/study2/TestHttpServer.java
@@ -3,9 +3,6 @@ package huarui.study2;
 import java.io.IOException;
 
 import org.junit.Test;
-
-import com.huarui.server.HttpServer;
-import com.huarui.server.HtttServlet;
 import com.huarui.servlet.MyHttpServlet;
 import com.huarui.util.ShowFileServerlet;
 
@@ -17,24 +14,11 @@ import com.huarui.util.ShowFileServerlet;
  * @createdate 2017年7月6日
  */
 public class TestHttpServer {
-
-	@Test
-	public void testHttpServer() throws Exception {
-		HttpServer hs = new HttpServer();
-		hs.await();
-	}
 	
 	@Test
 	public void testGetFileServlet() throws IOException{
 		ShowFileServerlet fs =new ShowFileServerlet();
 		System.out.println(fs.getFileServlet("D:\\"));
-	}
-	
-	@Test
-	public void testHttpServerlet(){
-		HtttServlet m = new HtttServlet();
-		m.startWorkerThreads();
-		m.await();
 	}
 	
 	@Test
