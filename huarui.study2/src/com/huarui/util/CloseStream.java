ce192be808b2b0e005d93364625c74266fc549f5
huarui <huarui@succez.com>
2017-07-19 09:23:33 +0800

    代码格式化

--- huarui.study2/src/com/huarui/util/CloseStream.java
+++ huarui.study2/src/com/huarui/util/CloseStream.java
@@ -2,8 +2,14 @@ package com.huarui.util;
 
 import java.io.Closeable;
 
+/**
+ * 关闭流或者Socket
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>succez</p>
+ * @author huarui
+ * @createdate 2017年7月18日
+ */
 public class CloseStream {
-	// 关闭流或者Socket  
 	public static void close(Closeable... closeables) {
 		if (closeables != null) {
 			for (Closeable closeable : closeables) {
