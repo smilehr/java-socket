82b81313cb320e932f04741e1de1116c8ba5d1ee
huarui <huarui@succez.com>
2017-07-19 09:19:57 +0800

    根据不同的url分发servlet类

---
+++ huarui.study2/src/com/huarui/server/Dispatcher.java
@@ -0,0 +1,35 @@
+package com.huarui.server;
+
+import java.io.File;
+import java.io.IOException;
+import com.huarui.intel.Request;
+import com.huarui.intel.Response;
+import com.huarui.servlet.InitPageServlet;
+import com.huarui.servlet.ShowFileServerlet;
+
+/**
+ * 根据请求的url分配相应的servlet
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>succez</p>
+ * @author Hr
+ * @createdate 2017年7月18日
+ */
+public class Dispatcher {
+	//定义webroot目录
+	public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+	public Response dispatch(Request request, Response response) throws IOException {
+		String url = request.getUrl();
+		if (new File(WEB_ROOT + url).exists()) {
+			response = InitPageServlet.initPage(WEB_ROOT + url);
+		}
+		if (new File(url).exists() && new File(url).isFile()) {
+			response = InitPageServlet.initPage(url);
+		}
+		if (url.equals("ShowFileServlet")) {
+			ShowFileServerlet sf = new ShowFileServerlet();
+			response = sf.getFileServlet(request.getParm(), response);
+		}
+		return response;
+	}
+}
